dda2b1f5536f32d64f43bf013a6eb150912ed124
tiger2005 <41613797+tiger2005@users.noreply.github.com>
2018-09-27 19:30:01 +0800

    Update procalc.cpp

--- procalc.cpp
+++ procalc.cpp
@@ -1,3 +1,4 @@
+//Testing in Bigint
 #include<cstdio>
 #include<cstring>
 #include<algorithm>
@@ -5,7 +6,8 @@
 using namespace std;
 namespace bint{
 	struct Bint{
-		int num[1012],len;
+		char num[1012];
+		int len;
 		bool bz;
 	};
 	Bint equal(char a[],int f,int l){
@@ -22,10 +24,10 @@ namespace bint{
 			if(a[i]=='.')	aa=i;
 			if(aa!=-1)	break;
 		}
-		if(aa==-1)	aa=l-13;
 		int now=11-l+aa;
+		if(aa==-1)	now=11;
 		for(int i=l;i>=f;i--){
-			if(a[i]!='.')	++now;
+			if(a[i]!='.')	++now; 
 			if(now>=0 && a[i]!='.')	c.num[now]=a[i]-'0';
 		}
 		c.len=now;
@@ -37,27 +39,27 @@ namespace bint{
 		if(a.bz)	printf("-");
 		for(int j=a.len;j>=12;j--)	printf("%d",a.num[j]);
 		if(a.len < 12)	printf("0");
-		if(i!=12){
+		if(i<12){
 			printf(".");
-			for(int j=11;j>=12-i;j--)	printf("%d",a.num[j]);
+			for(int j=11;j>=i;j--)	printf("%d",a.num[j]);
 		}
 		return;
 	}
 	int big(Bint a,Bint b){
 		if(a.bz^b.bz)	return !a.bz;
-		if(a.len!=b.len)	return a.len>b.len;
+		if(a.len!=b.len)	return ((a.len>b.len) ^ !a.bz)+1;
 		int bi=0;
 		for(int i=a.len;i>=0;i--){
 			if(a.num[i]>b.num[i]){
 				bi=1;
-				break;
+				goto P;
 			}
 			if(a.num[i]<b.num[i]){
-				bi=0;
-				break;
+				bi=2;
+				goto P;
 			}
 		}
-		if(bi && a.bz)	bi=3-bi;
+		P:if(bi && a.bz)	bi=3-bi;
 		return bi;
 	}
 	Bint plus(Bint a,Bint b);
@@ -82,9 +84,14 @@ namespace bint{
 				ans.num[i]%=10;
 			}
 		}
-		if(ans.num[lo])	++lo;
-		ans.len=lo-1;
+		if(ans.num[lo+1])	++lo;
+		ans.len=lo;
 		ans.bz=a.bz;
+		bool wh=false;
+		for(int i=0;i<=ans.len;i++){
+			if(ans.num[i])	wh=true;
+		}
+		if(ans.bz)	ans.bz=wh;
 		return ans;
 	}
 	Bint minus(Bint a,Bint b){
@@ -96,7 +103,9 @@ namespace bint{
 			ans.len=b.len;
 			return plus(a,ans);
 		}
-		if((big(a,b)==2) ^ !a.bz){
+		ans.bz=a.bz;
+		if((big(a,b)==2) ^ a.bz){
+			ans.bz=!ans.bz;
 			Bint t=b;
 			b=a;
 			a=t;
@@ -111,19 +120,108 @@ namespace bint{
 		}
 		while(!ans.num[lo])	--lo;
 		ans.len=lo;
-		ans.bz=(big(a,b)==1);
+		bool wh=false;
+		for(int i=0;i<=ans.len;i++){
+			if(ans.num[i])	wh=true;
+		}
+		if(ans.bz)	ans.bz=wh;
 		return ans;
 	}
+	Bint multiply(Bint a,Bint b){
+		Bint ans;
+		memset(ans.num,0,sizeof(ans.num));
+		int bl=0;
+		for(int i=0;i<=a.len;i++){
+			for(int j=0;j<=b.len;j++){
+				if(i+j<12)	continue;
+				ans.num[i+j-12]+=a.num[i]*b.num[j];
+				if(ans.num[i+j-12])	bl=max(bl,i+j-12);
+				while(ans.num[i+j-12]>=10){
+					++ans.num[i+j+1-12];
+					ans.num[i+j-12]-=10;
+					bl=max(bl,i+j+1-12);
+				}
+			}
+		} 
+		for(int i=0;i<1012;i++){
+			while(ans.num[i]>=10){
+				ans.num[i]-=10;
+				bl=max(bl,i+1);
+				++ans.num[i+1];
+			}
+		}
+		ans.len=bl;
+		ans.bz=a.bz^b.bz;
+		bool wh=false;
+		for(int i=0;i<=ans.len;i++){
+			if(ans.num[i])	wh=true;
+		}
+		if(ans.bz)	ans.bz=wh;
+		return ans;
+	} 
+	Bint divide(Bint a,Bint b){
+		Bint pl,ans;
+		memset(pl.num,0,sizeof(pl.num));
+		bool aaa=a.bz^b.bz; 
+		ans.bz=pl.bz=a.bz=b.bz=false;
+		memset(ans.num,0,sizeof(ans.num));
+		for(int i=a.len+11-b.len;i>=0;i--){
+			pl.num[i+1]=0;
+			pl.num[i]=1;
+			pl.len=i;
+			while(big(a,multiply(pl,b)) != 2){
+				ans=plus(ans,pl);
+				a=minus(a,multiply(pl,b));
+			}
+		}
+		ans.len=1011;
+		while(!ans.num[ans.len])	--ans.len;
+		ans.bz=aaa;
+		if(ans.num[0]<0)	ans.num[0]=0;
+		bool wh=false;
+		for(int i=0;i<=ans.len;i++){
+			if(ans.num[i])	wh=true;
+		}
+		if(ans.bz)	ans.bz=wh;
+		return ans;
+	}
+	Bint modulus(Bint a,Bint b){
+		Bint ans=divide(a,b);
+		for(int i=0;i<12;i++)	ans.num[i]=0;
+		if(ans.len<12)	ans.len=0;
+		bool aaa=a.bz;
+		a.bz=ans.bz=b.bz=false;
+		ans=minus(a,multiply(ans,b));
+		ans.bz=aaa;
+		bool wh=false;
+		for(int i=0;i<=ans.len;i++){
+			if(ans.num[i])	wh=true;
+		}
+		if(ans.bz)	ans.bz=wh;
+		return ans;
+	}
+	
 }
 using namespace bint;
 int main(){
-	char aa[10]={"123.456"},bb[15]={"123.456789"};
-	Bint b=equal(aa,0,6);
-	Bint c=equal(bb,0,9);
-	Bint a=minus(b,c);
+	char aa[1000],bb[1000];
+	scanf("%s %s",aa,bb);
+	Bint b=equal(aa,0,strlen(aa)-1);
+	Bint c=equal(bb,0,strlen(bb)-1);
+	printf("%d\n",big(b,c));
+	Bint a=plus(b,c);
 	print(a);
 	printf("\n");
-	print(b);
-	printf(" ");
-	print(c);
+	a=minus(b,c);
+	print(a);
+	printf("\n");
+	a=multiply(b,c);
+	print(a);
+	printf("\n");
+	a=divide(b,c);
+	print(a);
+	printf("\n");
+	a=modulus(b,c);
+	print(a);
+	printf("\n");
 }
